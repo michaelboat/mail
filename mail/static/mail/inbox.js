152d8842140b28a1e34016bf22745bde0d6bef29
Michael Boateng <112430340+michaelboat@users.noreply.github.com>
2025-08-10 22:33:18 -0400

    allowed users to send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // sending an email (POST request)
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +34,32 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+/**
+ * handles sending and email
+ */
+function send_email(event) {
+  event.preventDefault();
+
+  // store field content
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject =  document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // send data to the backend
+  fetch('/emails', {
+  method: 'POST',
+  body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+    load_mailbox('sent');
+  });
 }
